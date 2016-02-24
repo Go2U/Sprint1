ec27947c7ed233a59b0404462575c20cd32551a3
cesaralexander <cesaralexanderbonilla@gmail.com>
2016-02-24 17:04:11 -0500

    Falla del test

--- src/test/java/edu/eci/cosw/Go2U/Lab1ApplicationTest.java
+++ src/test/java/edu/eci/cosw/Go2U/Lab1ApplicationTest.java
@@ -1,5 +1,6 @@
 package edu.eci.cosw.Go2U;
 
+import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -9,10 +10,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Lab1Application.class)
 @WebAppConfiguration
-public class Lab1ApplicationTests {
+public class Lab1ApplicationTest {
 
 	@Test
 	public void contextLoads() {
+            fail("Información no encontrada");
 	}
 
 }
